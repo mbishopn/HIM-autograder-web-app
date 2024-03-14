c69ef8122e728178a9d67553df7e1a0d789cd230
DAX1207 <daksh12.vyas@gmail.com>
2024-03-14 09:34:22 -0400

    1

---
+++ Frontend/src/LandingPage.jsx
@@ -0,0 +1,16 @@
+export default function LandingPage() {
+  return (
+    <>
+      <h1>Landing Page</h1>
+      <div>
+        Welcome to HIM Auto-grader
+        <ul>
+          <li>Grading</li>
+          <li>Groups</li>
+          <li>Admin Users</li>
+          <li>Exit</li>
+        </ul>
+      </div>
+    </>
+  );
+}
